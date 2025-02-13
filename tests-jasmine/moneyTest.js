9545b78c0883244dac0e011b2c916dbcca7fdc05
Jithin Bathula <jithin.bathula@gmail.com>
2025-02-13 10:00:54 +0800

    Add Test cases using Jasmine

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: format currency", () => {
+    it('Test if currency is correctly converted form cents to dollars', () => {
+        expect(formatCurrency(2020)).toEqual('20.20')
+    });
+
+
+    it('Test with 0 dollars', () => {
+        expect(formatCurrency(0)).toEqual('0.00')
+    });
+
+})
--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -6,7 +6,7 @@ console.log("Test Suite: Format Currency")
 //base cases
 console.log("Test if currency is correctly converted form cents to dollars")
 
-if(formatCurrency(2020) == 20.20){
+if(formatCurrency(2020) == '20.20'){
     console.log("Passed")
 }else{
     console.log("failed")
